8495db744a3b9ccb6aaa437c64bd2ad858255ce0
feng19 <kevin@feng19.com>
2024-10-16 11:39:49 +0800

    add checksum-Elixir.Captcha.Native.exs

---
+++ checksum-Elixir.Captcha.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "captcha_nif-v0.2.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:6755791f7429b54ffc3812f8e2b073b6e51611d0dbf5829c1066dafe1ebb9b62",
+  "captcha_nif-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:4fb475209fdbe2deb6293c106d398aaf704f2ff9a73a9c1355852764525aab28",
+  "libcaptcha_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:488ef09ce1ec01a2dd151374a99a4259c72f5ef537be1e53a4202fb789e33f28",
+  "libcaptcha_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7d27f035459c68b8b0b3d8438548894b3630d6fd8276380877dd7fd813bb5067",
+  "libcaptcha_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:4e1bb8c5ab3599caa8a0a94db2d9aca74fa8ad749e72e100ca3d14ad3dcb48d2",
+  "libcaptcha_nif-v0.2.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:e88e3b3ae1f368fba32cb02e6ded05f092fadd17e75a87b7ae0c9c593a93432c",
+  "libcaptcha_nif-v0.2.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:2e28e60d1a883d10fadf73071951f750b6fcf6ff5e362e34cd2fcc26afae4e92",
+  "libcaptcha_nif-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:75be39ebb11082b41915bb014fff469b30788cf17aa808d85a24b0c348bb759c",
+  "libcaptcha_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:834b591816547188ffc085d434d9cc09666402a12fd5d374ef52b1c3dd3de5fc",
+  "libcaptcha_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:49f22b047eb94154aedd5ffc5016f1c55e74196b3de01b6f18a562a61588ebbf",
+}
